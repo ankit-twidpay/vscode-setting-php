c7cfba9049f5ec81f43f5f7734611a50845a37c7
ankit-twidpay <ankit.tiwari@twidpay.com>
2023-07-06 15:12:10 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,15 +18,30 @@ mv "$vscode_path" /Applications/
 # Update permissions
 chmod +x /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code
 
-# Add Visual Studio Code to PATH
-echo 'export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"' >> ~/.zshrc
+# Detect the shell profile file
+shell_profile=""
+if [ -f "$HOME/.zshrc" ]; then
+    shell_profile="$HOME/.zshrc"
+elif [ -f "$HOME/.bashrc" ]; then
+    shell_profile="$HOME/.bashrc"
+else
+    echo "Could not find .zshrc or .bashrc file."
+    exit 1
+fi
 
-# Source the .zshrc file to apply changes
-source ~/.zshrc
+# Add Visual Studio Code to PATH in the detected shell profile file
+echo 'export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"' >> "$shell_profile"
+echo "Visual Studio Code added to PATH in $shell_profile."
+
+# Source the detected shell profile file to apply changes
+source "$shell_profile"
 
 # Display success message
 echo "Visual Studio Code installed successfully."
 
+# Install extensions
 cat extensions.txt | xargs -L 1 code --install-extension
 
-cp setting.json ~/Library/Application\ Support/Code/User/settings.json
+# Copy settings file
+cp settings.json ~/Library/Application\ Support/Code/User/settings.json
+echo "VS Code settings copied to the appropriate location."
